c8d92fd3fe261ba948c9bb684738641b63a334da
janthony25 <junanthony.business@gmail.com>
2024-02-19 23:14:22 +1300

    Add CategoryContoller

---
+++ MVC-BulkyCompleteCRUD/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_BulkyCompleteCRUD.Data;
+using MVC_BulkyCompleteCRUD.Models;
+
+namespace MVC_BulkyCompleteCRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View();
+        }
+    }
+}
