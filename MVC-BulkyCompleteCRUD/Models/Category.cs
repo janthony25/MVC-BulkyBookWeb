7f3629be0a75d1535aae63201283319c39268ece
janthony25 <junanthony.business@gmail.com>
2024-02-19 22:51:57 +1300

    Add Data Annotation

--- MVC-BulkyCompleteCRUD/Models/Category.cs
+++ MVC-BulkyCompleteCRUD/Models/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace MVC_BulkyCompleteCRUD.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_BulkyCompleteCRUD.Models
 {
     public class Category
     {
+        [Key]
         public int ID { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
         public DateTime DateCreated { get; set; }
