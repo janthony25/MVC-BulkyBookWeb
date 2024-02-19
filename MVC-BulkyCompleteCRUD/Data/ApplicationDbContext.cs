a6710537ad0f5e83a350e1aea14e4c12a45c402a
janthony25 <junanthony.business@gmail.com>
2024-02-19 22:55:20 +1300

    Add ApplicationDbContext

---
+++ MVC-BulkyCompleteCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_BulkyCompleteCRUD.Models;
+
+namespace MVC_BulkyCompleteCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
