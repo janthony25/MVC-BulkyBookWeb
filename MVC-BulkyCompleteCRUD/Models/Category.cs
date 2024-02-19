d0808fd70a367268ff3f56a4491faf323a4bb25a
janthony25 <junanthony.business@gmail.com>
2024-02-19 22:51:35 +1300

    Add Category

---
+++ MVC-BulkyCompleteCRUD/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace MVC_BulkyCompleteCRUD.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
