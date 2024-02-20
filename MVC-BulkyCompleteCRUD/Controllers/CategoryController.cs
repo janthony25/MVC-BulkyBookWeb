8c4f45b92e42113cfcbdd0a73edd27474220c18e
janthony25 <junanthony.business@gmail.com>
2024-02-20 18:55:25 +1300

    Add Category Frontend View

--- MVC-BulkyCompleteCRUD/Controllers/CategoryController.cs
+++ MVC-BulkyCompleteCRUD/Controllers/CategoryController.cs
@@ -65,6 +65,43 @@ namespace MVC_BulkyCompleteCRUD.Controllers
         }
 
 
+        // GET
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+
+            var obj = _db.Categories.Find(id);
+            if (obj == null)
+            {
+
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
 
     }
 }
